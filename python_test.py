5428ad83da2f0d1c5d3951a6c61db1e6ad1c5d50
Hanna <h.israelsson98@gmail.com>
2023-07-10 09:39:17 +0200

    Multiple changes and additions to file. Had to reset commits, so this one is bigger than intended.

--- python_test.py
+++ python_test.py
@@ -6,29 +6,37 @@ from os import makedirs
 from datetime import date
 
 def segmentation(image_folder_path, model_path, diam = 40, save = False, savedir=None):
-    # dir_path is the path to the 
+    # savedir is the directory in which to save the masks
     model = models.CellposeModel(gpu = True, pretrained_model=model_path)
 
     files = get_image_files(image_folder_path, 'stupid_variable')
     imgs = [imread(f) for f in files]
+    names = [str(f) for f in files]
 
     masks, flows, styles = model.eval(imgs, diameter=diam, channels = [0,0], 
                                             flow_threshold=0.4, do_3D = False)
 
     if save:
-        # Create a directory where the files can be saved
-        if savedir == None:
-            savedir = "GeneratedMasks_"+str(date.today())
-        makedirs(savedir)
+        _save(savedir, imgs, masks, flows, names)
 
-        # Create an array of names for the files.
-        names = np.arange(len(imgs)).astype(str)
-        names = np.char.zfill(names, 4)
+    return masks, savedir
 
-        save_masks(imgs, masks, flows, "GeneratedMasks", png=False, tif=True, 
-                   savedir=savedir, save_txt=False)
-    return masks
 
+def _save(savedir, imgs, masks, flows, names):
+    # Create a directory where the files can be saved
+    if savedir == None:
+        savedir = "GeneratedMasks_"+str(date.today())
+    makedirs(savedir)
+
+    # masks_flows_to_seg
+    save_masks(imgs, masks, flows, names, png=False, tif=True, 
+               savedir=savedir, save_txt=False)
+    
+    return None
+
+
+def get_no_of_roi(masks):
+    return [np.max(m) for m in masks]
 
 
 def main():
@@ -36,7 +44,15 @@ def main():
     # image_folder_path = "//storage3.ad.scilifelab.se/alm/BrismarGroup/Hanna/Data_from_Emma/onehourconfluent"
     model_path = 'C:/Users/workstation3/Documents/CP_20230705_confl'
 
-    segmentation(image_folder_path, model_path, save = True)
+    masks, savedir = segmentation(image_folder_path, model_path, save = False)
+
+    print(get_no_of_roi(masks))
 
 if __name__ == "__main__":
     main()
+
+
+
+
+# How to continue: Track cells. Change the values in each mask so that a certain cell has a certain value?
+# This will be complicated. Will it take to much work?
