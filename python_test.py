d726cf8b6c2ecef20ce42e9c33790552d3503bff
Hanna <h.israelsson98@gmail.com>
2023-07-10 11:16:27 +0200

    Added function for loading ready-made masks and some new documentation.

--- python_test.py
+++ python_test.py
@@ -4,21 +4,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 from os import makedirs
 from datetime import date
+import glob
 
 def segmentation(image_folder_path, model_path, diam = 40, save = False, savedir=None):
-    # savedir is the directory in which to save the masks
-    model = models.CellposeModel(gpu = True, pretrained_model=model_path)
+    """Savedir is the directory in which to save the masks"""
 
-    files = get_image_files(image_folder_path, 'stupid_variable')
+    # Get the model
+    model = models.CellposeModel(gpu = True, pretrained_model=model_path)
+    # Open image files
+    files = get_image_files(image_folder_path, 'unused_mask_filter_variable')
     imgs = [imread(f) for f in files]
     names = [str(f) for f in files]
-
+    # Segment images
     masks, flows, styles = model.eval(imgs, diameter=diam, channels = [0,0], 
                                             flow_threshold=0.4, do_3D = False)
-
+    # Save masks as .tif's in folder savedir
     if save:
         _save(savedir, imgs, masks, flows, names)
-
     return masks, savedir
 
 
@@ -27,24 +29,35 @@ def _save(savedir, imgs, masks, flows, names):
     if savedir == None:
         savedir = "GeneratedMasks_"+str(date.today())
     makedirs(savedir)
-
-    # masks_flows_to_seg
+    # Save the masks in said directory
     save_masks(imgs, masks, flows, names, png=False, tif=True, 
                savedir=savedir, save_txt=False)
-    
     return None
 
 
 def get_no_of_roi(masks):
+    """Get the number of RIO's in each mask"""
     return [np.max(m) for m in masks]
+
+
+def get_mask_files(folder):
+    """Load ready-made masks from specified folder."""
+    file_names = glob.glob(folder + '/*_masks.tif')
+    masks = [imread(mask) for mask in file_names]
+    return masks
 
 
 def main():
     image_folder_path = "//storage3.ad.scilifelab.se/alm/BrismarGroup/Hanna/Data_from_Emma/Confluent_images/short"
     # image_folder_path = "//storage3.ad.scilifelab.se/alm/BrismarGroup/Hanna/Data_from_Emma/onehourconfluent"
-    model_path = 'C:/Users/workstation3/Documents/CP_20230705_confl'
+    model_path = 'C:/Users/workstation3/Documents/Hanna/Master2023/CP_20230705_confl'
 
-    masks, savedir = segmentation(image_folder_path, model_path, save = False)
+    # masks, savedir = segmentation(image_folder_path, model_path, save = False)
+    masks = get_mask_files('GeneratedMasks_2023-07-07')
+
+    print(masks)
+    print(type(masks))
+    print(len(masks))
 
     print(get_no_of_roi(masks))
 
