0fef019202e23fd915621f49258c36d7b4ab31e0
Hanna <h.israelsson98@gmail.com>
2023-07-24 16:03:48 +0200

    Edited correlation function and start on correlation by distance function

--- python_test.py
+++ python_test.py
@@ -62,19 +62,18 @@ def get_roi_count(masks: list) -> list:
     return [len(np.unique(m))-1 for m in masks] # Have to take -1 bc regions with 0 do not count as roi:s
 
 
-# def open_masks(folder: str) -> list:
-#     """Load ready-made masks from specified folder."""
-#     file_names = glob.glob(folder + '/*_masks.tif')
-#     masks = [imread(mask) for mask in file_names]
-#     return masks
+def open_masks(folder: str) -> list:
+    """Load ready-made masks from specified folder."""
+    file_names = glob.glob(folder + '/*_masks.tif')
+    masks = [imread(mask) for mask in file_names]
+    return masks
 
-def open_masks(file_path):
-    return imread(file_path)
+# def open_masks(file_path):
+#     return imread(file_path)
 
 
 def get_centers_of_mass(masks: list) -> Tuple[list, list]:
-    """Returns a list with coordinates for centers of mass for each cell in each image
-    on the form [[(coordinate of c1, im1), (coordinates)]]"""
+    """Returns a list with coordinates for centers of mass for each cell in each image."""
     coms = []
     roi_count = get_roi_count(masks)
     for i in range(len(masks)):
@@ -149,25 +148,56 @@ def plot_cell_intensities(cell_numbers: list, tracked_cells: list, images: list)
     return None
 
 
-def correlation(tracked_cells, images, cell_numbers=None, all_cells=False, plot=False):
+def correlation(tracked_cells, images, cell_numbers=None, all_cells=False, plot=True):
     intensities = []
     if all_cells:
-        cell_numbers = range(np.max(tracked_cells))
-    corrcoefs = np.zeros(len(cell_numbers), len(cell_numbers))
+        cell_numbers = range(1, np.max(tracked_cells)+1)
+    corrcoefs = np.zeros((len(cell_numbers), len(cell_numbers)))
     
-    for cell_number in cell_numbers:
+    for cell_number, j in zip(cell_numbers, range(len(cell_numbers))):
         intensities.append(get_cell_intensities(cell_number, tracked_cells, images))
-        for i in range(cell_number+1):
-            corrcoefs[i, cell_number] = np.correlate(i, cell_number)
-    # corrcoefs = np.corrcoef(intensities)
+        for i in range(j+1):
+            corrcoefs[i, j] = np.correlate(intensities[i], intensities[j])
 
     if plot:
-        plt.matshow(corrcoefs)
+        if all_cells:
+            idx = np.round(np.linspace(0, len(list(cell_numbers)) - 1, 10, dtype='int'))
+            tick_labels = [str(c) for c in idx]
+        else:
+            idx = cell_numbers
+            tick_labels = [str(c) for c in cell_numbers]
+        print(tick_labels)
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        cax = ax.matshow(corrcoefs)
         plt.title("Correlation coefficients")
-        plt.colorbar(label="Correlation", orientation="vertical", fraction=0.046, pad=0.04)
+        fig.colorbar(cax, label="Correlation", orientation="vertical")#, fraction=0.046, pad=0.04)
+        ax.set_xticks(idx)
+        ax.set_yticks(idx)
+        ax.set_xticklabels(tick_labels)
+        ax.set_yticklabels(tick_labels)
         plt.show()
 
     return corrcoefs
+
+
+def get_stable_cells(tracked_masks):
+    """Returns the cell numbers that are common for all images."""
+    numbers_lists = []
+    for image in tracked_masks:
+        numbers_lists.append(np.unique(image))
+    commons = numbers_lists[0]
+    for i in range(len(numbers_lists)):
+        commons = np.intersect1d(commons, numbers_lists[i+1])
+    return commons
+    
+
+def correlation_by_distance(ref_cell: int, tracked_masks, images, plot=True):
+    # Only use cells that appear in all images.
+    coms, roi_count = get_centers_of_mass(tracked_masks[0])
+    return None
+
+
 
 
 def main():
@@ -175,21 +205,20 @@ def main():
     # image_folder_path = "//storage3.ad.scilifelab.se/alm/BrismarGroup/Hanna/Master2023/2023-07-11-imaging-2/2023-07-11/Ouabain_image_stack/short"
     # image_folder_path = "//storage3.ad.scilifelab.se/alm/BrismarGroup/Hanna/Master2023/2023-07-11-imaging-2/2023-07-11/CBX-ouabain-10.tif"
     # image_folder_path = "//storage3.ad.scilifelab.se/alm/BrismarGroup/Hanna/Data_from_Emma/onehourconfluent/onehourrecording-hbss-nd5-10percent.tif"
+    image_folder_path = "C:/Users/hisra/Documents/Master 2023/Master2023/short"
     # model_path = 'C:/Users/workstation3/Documents/Hannas_models/CP_20230705_confl'
     # model_path = "C:/Users/workstation3/Documents/Hannas_models/CBXoua202307"
 
     # masks = open_masks("onehourconfluent-tracking-from-separate-files_masks.tif")
-    # masks = open_masks("onehourconfluent-tracking-from-separate-files_masks.tif")
+    masks = open_masks("C:/Users/hisra/Documents/Master 2023/Master2023/NewMasks_2023-07-13")
     # masks, savedir = segmentation(image_folder_path, model_path, save = False)
 
-    # images0, image_names = open_images(image_folder_path)
-    # # print(type(images0), len(images0))
-
+    images, image_names = open_images(image_folder_path)
 
     # images, image_name = open_images(image_folder_path)
 
     # tracked_masks = track_cells_com(masks, name="onehourconfluent-tracking-from-single-file", save=True)
-    # corrcoefs = correlation(tracked_masks, images, all_cells=True, plot=True)
+    corrcoefs = correlation(masks, images, all_cells=True, plot=True)
 
     # plot_cell_intensities([54,55,56,57, 58, 59, 60, 61], masks, images)
 
